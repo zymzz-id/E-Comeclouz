810852522946dd0fa89abfc6096cffe20ea6a8ab
Zymzz <srepettsrepett85@gmail.com>
2026-07-10 11:18:17 +0800

    Add API module with Axios configuration

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:4000/api',
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('admin_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
